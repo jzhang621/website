5d8ba1bbf6d22fa26f7de6d325fa14d653af3d33
Jimmy Zhang <jzhang621@gmail.com>
2025-07-08 12:52:21 -0400

    move Nav.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { Nav } from "./components/Nav";
+import { Nav } from "../components/Nav";
 import { FootnoteProvider } from "../components/Footnote";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 
