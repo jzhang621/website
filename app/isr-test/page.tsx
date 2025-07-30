926d29efc28d7ed762424290e957878a5f768876
Jimmy Zhang <jzhang621@gmail.com>
2025-07-30 11:01:55 -0400

    a

--- app/isr-test/page.tsx
+++ app/isr-test/page.tsx
@@ -1,6 +1,6 @@
 export const revalidate = 60;
 
 export default async function ISRTest() {
-  console.log("Hello, where will this be logged? Expecting to see this as a background job.");
+  //   console.log("Hello, where will this be logged? Expecting to see this as a background job.");
   return <div>ISR Test</div>;
 }
